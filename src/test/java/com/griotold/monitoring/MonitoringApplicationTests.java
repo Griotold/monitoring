740e6bb10f673df68c95602b5addd3405ca75152
Griotold <gotjd9773@naver.com>
2024-12-19 15:31:41 +0900

    Initial commit

---
+++ src/test/java/com/griotold/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griotold.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
